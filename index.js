04c24bf14bbc74a11315b2d3009a058c39a1fd3d
Balicea28 <b.alicea1998@gmail.com>
2022-06-06 11:43:09 -0400

    commit tree a1ce37a8f20b3145e41cd2f2494ebaf62ba2aebb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0 , -2 );
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+function createFareMultiplier(){
+const fareMultiplier = function (fare){
+    return fare * fareMultiplier;
+}
+}
